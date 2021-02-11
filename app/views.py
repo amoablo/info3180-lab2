ddf4ef1afe166e5bd501240662709dde5c50fcd0
Your Name <you@example.com>
2021-02-11 17:38:58 -0500

    Finished

--- app/views.py
+++ app/views.py
@@ -7,11 +7,21 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
 # Routing for your application.
 ###
+
+@app.route('/profile')
+def profile():
+    date_format = format_date_joined(datetime.datetime.now() )
+    return render_template('profile.html',date=date_format)
+
+
+def format_date_joined(date):
+    return ("Joined "+ date.strftime("%B, %Y"))
 
 @app.route('/')
 def home():
@@ -22,7 +32,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Kyle Sterling")
 
 
 ###
